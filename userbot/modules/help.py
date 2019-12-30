b42deed9a17d3de0b151007761ac87f3d3a1d8f6
MHMDFJRMLN <47522894+MuhamadFajarGaray@users.noreply.github.com>
2019-12-30 17:45:40 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -23,6 +23,6 @@ async def help(event):
             \nUsage: .help <module name>")
         string = ""
         for i in CMD_HELP:
-            string += "`" + str(i)
-            string += "`\n"
+            string += "`" + str(i) + "`"
+            string += " >•< "
         await event.reply(string)

b0974756fb948f4b2bc17448e732c06ddad4887e
MHMDFJRMLN <47522894+MuhamadFajarGaray@users.noreply.github.com>
2019-12-30 17:59:03 +0700

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -231,8 +231,7 @@ async def kang(args):
                 # Ensure user doesn't get spamming notifications
                 await bot.send_read_acknowledge(conv.chat_id)
 
-        await args.edit(f"`Kanged successfully!`\
-            \ncan be found [here](t.me/addstickers/{packname})",
+        await args.edit(f"`Success!.` get [here](t.me/addstickers/{packname}) buddy...",
                         parse_mode='md')
 
 

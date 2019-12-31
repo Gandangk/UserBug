f1bfeecf1002aac7f699c5dc131e23d7e968e6b6
MHMDFJRMLN <47522894+MuhamadFajarGaray@users.noreply.github.com>
2019-12-31 19:15:18 +0700

    edit heroku logs when your userbot is alive

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -26,12 +26,11 @@ except PhoneNumberInvalidError:
 for module_name in ALL_MODULES:
     imported_module = import_module("userbot.modules." + module_name)
 
-LOGS.info("You are running OpenUserBot [v7.7]")
+LOGS.info("You are running UserBug [v1.0]")
 
 LOGS.info(
-    "Congratulations, your userbot is now running !! Test it by typing .on in any chat."
-    "Once died , but now ON again, A BIG thanks to https://github.com/Devp73 Dev Patel who have helped make this userbot ON AGAIN with his HUGE FIX!!"
-    "If you need assistance, head to https://t.me/PPE_Support")
+    "Congrats, your userbug is now running!! Type .alive for sure."
+    "If you need something, ask on https://t.me/UserBotIndo")
 
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
